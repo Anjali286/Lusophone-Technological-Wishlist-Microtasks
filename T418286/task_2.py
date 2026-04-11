e224593abbc4398a525bf77f79c890a39a388273
Adwivedii <anjalidwivediii28@gmail.com>
2026-04-12 01:42:56 +0530

    Fix: Simplify the code

--- T418286/task_2.py
+++ T418286/task_2.py
@@ -1,338 +1,122 @@
 import requests
 import csv
 import sys
-import re
-import time
-from urllib.parse import quote, urlparse
-from datetime import datetime
-from colorama import init, Fore, Style
+from urllib.parse import urlparse, urlencode, parse_qs
 
-#configure
-time_out=10
-max_url_length=2000
-max_internet_retries=5
-initial_wait=5
-max_wait=60
-max_url_retries=3
+# Configure
+time_out = 5
+# Input filename
+input_file = "Task 2 - Intern.csv"
 
-request_header={
+request_header = {
     "User-Agent": (
-        "Mozilla/5.0 (Windows NT 11.0; Win64; x64) "
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/134.0.0.0 Safari/537.36"
-    ) 
+        "Chrome/147.0.7727.55 Safari/537.36"
+    )
 }
 
-# To reset color and initialize colorama
-init(autoreset=True)
-
-# Add timestamp to csv output file 
-time_stamp=datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-output_CSV=f"results{time_stamp}.csv"
-
-# Add color to status code
-def color_status(status_code):
-  ORANGE="\033[38;5;208m"
-  GREY=Style.BRIGHT + Fore.BLACK
-
-  if isinstance(status_code,int):
-    if 200<=status_code<300:
-      return Fore.GREEN
-    elif 300<=status_code<400:
-      return Fore.YELLOW
-    elif 400<=status_code<500:
-      return ORANGE
-    elif status_code>=500:
-      return Fore.RED
-  if status_code in ("Timeout", "Connection Error", "SSL Error","Too Many Redirects"):
-    return Fore.MAGENTA
-  if status_code=="Domain not found":
-    return GREY
-  if status_code=="Invalid URL":
-    return Fore.YELLOW
-
-  return Fore.RED
-
-# Output csv file and summary
-def get_category(status_code):
-  if isinstance(status_code,int):
-    if 200<=status_code<300:
-      return "Working"
-    elif 300<=status_code<400:
-      return "Redirected"
-    elif 400<=status_code<500:
-      return "Client Error"
-    elif status_code>=500:
-      return "Server Error"
-  if status_code=="Timeout":
-    return "Timeout"
-  if status_code=="Connection Error":
-    return "Connection Error"
-  if status_code=="Domain not found":
-    return "Domain not found"
-  return "Unknown Error"
-
-
-results_log=[]
-def log_result(status_code, url):
-  category=get_category(status_code)
-  results_log.append({
-      "status_code":str(status_code),
-      "url":url,
-      "category":category
-  })
-
-def print_result(status_code,url,current,total):
-  color=color_status(status_code)
-  print(color+ f"[{current}/{total}] ({status_code}) {url}")
-  log_result(status_code,url)
-
-def print_summary():
-  counts={
-      "Working":0,
-      "Redirected":0,
-      "Client Error":0,
-      "Server Error":0,
-      "Timeout":0,
-      "Connection Error":0,
-      "Domain not found":0,
-      "Unknown Error":0,
-  }
-  for result in results_log:
-    category=result["category"]
-    if category in counts:
-      counts[category]+=1
-    else:
-      counts["Unknown Error"]+=1
-
-  total=len(results_log)
-
-  print("\nSummary:")
-  print(f"Total URL processed: {total}")
-  print(f"Working (2xx) : {counts['Working']}")
-  print(f"Redirected (3xx) : {counts['Redirected']}")
-  print(f"Client Error (4xx) : {counts['Client Error']}")
-  print(f"Server Error (5xx) : {counts['Server Error']}")
-  print(f"Timeout : {counts['Timeout']}")
-  print(f"Connection Error : {counts['Connection Error']}")
-  print(f"Domain not found : {counts['Domain not found']}")
-  print(f"Unknown Error : {counts['Unknown Error']}")
-
-
-# Save results
-def save_results():
-  try:
-    with open(output_CSV,"w",newline="",encoding="utf-8") as f:
-      writer=csv.DictWriter(f,fieldnames=["status_code","url","category"])
-      writer.writeheader()
-      writer.writerows(results_log)
-    print(f"Results saved: {output_CSV}")
-  except Exception as e:
-    print(f"Could not save CSV:{e}")
-
-#functions to handle edge cases
-# Internet Connection
-# Check random url to check the Internet, true if connected, false otherwise
-def check_internet():
-  try:
-    requests.get("https://www.google.com",timeout=5,headers=request_header)
-    return True
-  except Exception:
-    return False
-
-# Wait for internet restoration when drops during execution
-# Retries upto max_internet_retries, if internet doesn't restore, exits script
-def wait_for_internet(failedURL):
-  print(f"Internet Connection Lost")
-  print("Rechecking")
-  wait_time=initial_wait
-  attempt=1
-  while attempt<=max_internet_retries:
-    time.sleep(wait_time)
-    if(check_internet()):
-      print(f"\n Internet restored after {attempt} attempt(s)\n")
-      return True
-    wait_time=min(wait_time*2,max_wait)
-    attempt+=1
-
-  print(Fore.RED+f"No Internet")
-  sys.exit()
-
-# Handle URL based test cases
-
-# Check whether URL is valid or just a random text(invald)
-def is_valid(url):
-  if not url.startswith("http"):
-    return False
-  if "." not in url:
-    return False
-  return True
-
-# Check if url is a localhost url
-def is_local_url(url):
-  local_patterns=["localhost","127.0.0.1","192.168","10.0"]
-  return any(pattern in url for pattern in local_patterns)
-
-#Check if URL uses a raw IP address instead of domain
-def is_IP_url(url):
-  ip_pattern=r'https?://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-  return bool(re.search(ip_pattern,url))
-
-# Normalize url for duplicate detection, removing http/https, www or slashes
-def normalize(url):
-  url=url.strip();
-  url=url.replace("https://","").replace("http://","")
-  url=url.replace("www.","")
-  url=url.rstrip("/")
-  return url.lower()
+# Handle URL based edgecases
 
 # Adding protocol prefix if missing
-def fix_missing(url):
-  if not url.startswith("http://") and not url.startswith("https://"):
-    url="https://"+url
-  return url
-
-# Check if url has special characters
-def clean_encoding(url):
-  try:
-    parsed=urlparse(url)
-    clean=parsed._replace(path=quote(parsed.path, safe="/:@!$&'()*+,;="))
-    return clean.geturl()
-  # if cleaning fails, return original
-  except Exception:
+def fix_missing_url(url):
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = "https://"+url
     return url
-
-# Search all rows for URL
-def find_url(row):
-  for cell in row:
-    if cell.strip().startswith("http"):
-      return cell.strip()
-  return None
-
-# Final url check (also handles timeouts, redirects)
+    
+# Attempt to fetch URL and print the status code or specific error message
 def check_url(url, current, total):
-  for attempt in range(1,max_url_retries+1):
     try:
-      response=requests.get(url,timeout=time_out,allow_redirects=True,headers=request_header)
-      print_result(response.status_code,url,current,total)
-      return
-
-    # print exceptions
+        response = requests.get(url, timeout = time_out, allow_redirects = True, headers = request_header)
+        print(f"[{current}/{total}] ({response.status_code}) {url}")
+        return
+    
+    # print specific exceptions
     except requests.exceptions.MissingSchema:
-      print_result("Invalid URL", url, current, total)
-      return
+        print(f"[{current}/{total}] (Invalid URL: Missing Schema) {url}")
+        return
     except requests.exceptions.InvalidURL:
-      print_result("Invalid URL", url, current, total)
-      return
+        print(f"[{current}/{total}] (Invalid URL: Malformed) {url}")
+        return
     except requests.exceptions.SSLError:
-      print_result("SSL Error", url, current, total)
-      return
+        print(f"[{current}/{total}] (SSL Error) {url}")
+        return
     except requests.exceptions.TooManyRedirects:
-      print_result("Too Many Redirects", url, current, total)
-      return
+        print(f"[{current}/{total}] (Too Many Redirects) {url}")
+        return
     except requests.exceptions.Timeout:
-      print_result("Timeout", url, current, total)
-      return
+        print(f"[{current}/{total}] (Timeout) {url}")
+        return
     except requests.exceptions.ConnectionError as e:
-      if not check_internet():
-        restored=wait_for_internet(url)
-        if attempt<max_url_retries:
-          continue
-        else:
-          print(Fore.RED + f"(No Internet Connection - Maximum Retries Reached) {url}")
-          return
-      else:
         if "NameResolutionError" in str(e) or "Failed to resolve" in str(e):
-          print_result("Domain not found", url, current, total)
+          print(f"[{current}/{total}] (DNS Resolution Error) {url}")
           return
         else:
-          print_result("Connection Error", url, current, total)
+          print(f"[{current}/{total}] (Connection error) {url}")
           return
     except Exception as e:
-      print_result(f"Unknown Error", url, current, total)
+      print(f"[{current}/{total}] (Unknown Error - {str(e)[:60]}) {url}")
       return
+    
+#Normalize url for duplicate detection, removing http/https, www or slashes
+def normalize(url):
+    url = url.strip();
+    url = url.replace("https://","").replace("http://","")
+    url = url.replace("www.","")
+    url = url.rstrip("/")
+    return url.lower()
+
 
 def main():
+    try:
+        with open(input_file, "r", encoding = "utf-8-sig") as file:
+          reader = csv.DictReader(file)
+          rows_to_process = list(reader)
+    except FileNotFoundError:
+        print(f"Error: File '{input_file}' not found.")
+        sys.exit()
+    
+    # If no rows to process, print message and exit
+    if not rows_to_process:
+        print(f"File '{input_file}' is empty.")
+        file.close()
+        sys.exit()
+    
+    current = 0
+    total = len(rows_to_process)
 
-  if not check_internet():
-    wait_for_internet("Startup")
-    print("Internet connection confirmed")
+    visited_urls = set()
 
-  try:
-    file=open("Task 2 - Intern.csv","r",encoding="utf-8-sig")
-  except FileNotFoundError:
-    print(" File Not Found")
-    sys.exit()
+    for row in rows_to_process:
+  
+        # Skip empty row
+        if not row:
+          continue
 
-  reader=csv.reader(file)
-  try:
-    # Skip header row if does not contain a URL
-    header=next(reader)
-    if header and header[0].strip().startswith("http"):
-      rows_to_process=[header]
-    else:
-      rows_to_process=[]
-  except StopIteration:
-    print("No URLs found")
-    file.close()
-    sys.exit()
+        url = row.get("urls", "")
 
-  rows_to_process+=list(reader)
-  file.close()
+        # Remove whitespaces from url
+        url=url.strip()
 
-  # If file contain only header
-  if not rows_to_process:
-    print("Only header found. No URLs found")
-    sys.exit()
+        if not url:
+          continue
 
-  total=len(rows_to_process)
-  current=0;
-  visited_urls=set()
+        # Skip if not a URL
+        if not url.startswith("http") and "." not in url:
+          continue
 
-  for row in rows_to_process:
-    current+=1
-    # Skip empty row
-    if not row:
-      continue
+        # Add protocol prefix if missing
+        url = fix_missing_url(url)
 
-    url=find_url(row)
-    if url is None:
-      if row[0].strip()=="":
-        continue
-    # Remove whitespaces
-    url=url.strip()
+        # Normalize url, check for duplicates, and skip it with a warningif already visited
+        normalized_url = normalize(url)
+        if normalized_url in visited_urls:
+          continue
+        visited_urls.add(normalized_url)
 
-    if not url:
-      continue
-    if not url.startswith("http") and "." not in url:
-      continue
+        # Visit URL and print status code or error message
+        current += 1
+        check_url(url, current, total)
 
-    # Add protocol prefix
-    url=fix_missing(url)
-
-    if len(url)>max_url_length:
-      print("Very Long URL")
-    if is_local_url(url):
-      print("Local/Internal URL")
-    if is_IP_url(url):
-      print("IP based URL")
-    if not is_valid(url):
-      print("Invalid URL")
-
-    # Remove special characters
-    url=clean_encoding(url)
-    # Check for duplicates
-    normalized=normalize(url)
-    if normalized in visited_urls:
-      continue
-    visited_urls.add(normalized)
-
-    # Visit URLs
-    check_url(url,current,total)
-    time.sleep(2) # To rate limit and to avoid overwhelming servers 
-
-  print_summary()
-  save_results()
-
-if __name__=="__main__":
-  main()
+if __name__ == "__main__":
+    main()
